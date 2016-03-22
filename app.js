0a84bb2340888804c826a5cff35b802ee7f01a85
marcusps11 <marcus@1roof.com>
2016-03-22 19:38:33 +0000

    exercise 2 complete

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 // console.log a # sign for each value
 // then minus one off the 
 
-
-
-
-
-var line = "#"
-
-while (line.length <= 7){
-  console.log(line)
-  line += "#"
+for(var i = 1 ; i <= 100 ; i ++) {
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log('fizzbuzz')
+  }
+  else if (i % 3 === 0) {
+    console.log("Fizz")
+  } else if (i % 5 == 0 ){ 
+    console.log("Buzz")
+  } else {
+    console.log(i)
+  }
 }
 
 
