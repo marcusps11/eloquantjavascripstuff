f64d357c0b0c4927501efc803b49a49da60830f8
marcusps11 <marcus@1roof.com>
2016-03-22 19:26:01 +0000

    triangles exercise

---
+++ app.js
@@ -0,0 +1,17 @@
+// for every value in the triangles variable 
+// console.log a # sign for each value
+// then minus one off the 
+
+
+
+
+
+var line = "#"
+
+while (line.length <= 7){
+  console.log(line)
+  line += "#"
+}
+
+
+
