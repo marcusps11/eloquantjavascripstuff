d435238307fa0526e27d4f3e66b597c1c784deb4
marcusps11 <marcus@1roof.com>
2016-03-23 18:51:05 +0000

    chapter 2 exercise 1

--- app.js
+++ app.js
@@ -2,18 +2,13 @@
 // console.log a # sign for each value
 // then minus one off the 
 
-for(var i = 1 ; i <= 100 ; i ++) {
-  if (i % 3 === 0 && i % 5 === 0){
-    console.log('fizzbuzz')
-  }
-  else if (i % 3 === 0) {
-    console.log("Fizz")
-  } else if (i % 5 == 0 ){ 
-    console.log("Buzz")
+
+function min(arg, arg2){
+  if(arg < arg2){
+    console.log(arg)
   } else {
-    console.log(i)
+    console.log(arg2)
   }
 }
 
-
-
+min(0,-10)
